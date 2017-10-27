73860bd32cf9c22d2694ea0f0e245b8df4bfd8b6
Jörg <joerg.sesterhenn@gmail.com>
2017-10-27 23:27:54 +0200

    initial commit of archetype

---
+++ pi-server/src/main/java/de/chickenpi/ChickenPiServerApplication.java
@@ -0,0 +1,29 @@
+package de.chickenpi;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class ChickenPiServerApplication extends Application<ChickenPiServerConfiguration> {
+
+    public static void main(final String[] args) throws Exception {
+        new ChickenPiServerApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "chickenpi server";
+    }
+
+    @Override
+    public void initialize(final Bootstrap<ChickenPiServerConfiguration> bootstrap) {
+        // TODO: application initialization
+    }
+
+    @Override
+    public void run(final ChickenPiServerConfiguration configuration,
+                    final Environment environment) {
+        // TODO: implement application
+    }
+
+}

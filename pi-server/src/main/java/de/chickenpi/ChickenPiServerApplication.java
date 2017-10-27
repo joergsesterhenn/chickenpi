f0e6b7d865c50a8460bc0bf08ac7862472d75901
Jörg <joerg.sesterhenn@gmail.com>
2017-10-28 01:40:07 +0200

    added sonarcloud

--- pi-server/src/main/java/de/chickenpi/ChickenPiServerApplication.java
+++ pi-server/src/main/java/de/chickenpi/ChickenPiServerApplication.java
@@ -1,11 +1,15 @@
 package de.chickenpi;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.chickenpi.rest.controller.EmployeeRESTController;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
 public class ChickenPiServerApplication extends Application<ChickenPiServerConfiguration> {
-
+	 private static final Logger LOGGER = LoggerFactory.getLogger(ChickenPiServerApplication.class);
     public static void main(final String[] args) throws Exception {
         new ChickenPiServerApplication().run(args);
     }
@@ -23,7 +27,8 @@ public class ChickenPiServerApplication extends Application<ChickenPiServerConfi
     @Override
     public void run(final ChickenPiServerConfiguration configuration,
                     final Environment environment) {
-        // TODO: implement application
+    	 LOGGER.info("Registering REST resources");
+         environment.jersey().register(new EmployeeRESTController(environment.getValidator()));
     }
 
 }
